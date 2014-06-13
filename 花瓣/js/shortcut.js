604ef4c5a3c64cc4026fa607c4b0e2e415aa12b6
foolof41 <foolof41@gmail.com>
2014-06-13 14:34:10 +0800

    demos

---
+++ 花瓣/js/shortcut.js
@@ -0,0 +1,1 @@
+"use strict";(function(){var a=function(b){var c=b.keyCode;if(b.ctrlKey&&b.shiftKey&&c>64&&c<91){b.preventDefault();chrome.runtime.sendMessage({msg:"capture_hotkey",keyCode:c})}};if(document.body.hasAttribute("huaban_collector_injected")){return}document.body.setAttribute("huaban_collector_injected",true);document.body.addEventListener("keydown",a,false)}());
